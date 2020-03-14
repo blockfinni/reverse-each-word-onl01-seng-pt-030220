4d66c49d3678b0efd0122f37d52f2260fb6d2d99
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-14 04:48:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,8 @@ def reverse_each_word(array)
   array2 = array1.each {|x| x.reverse!}
   return array2.join(" ")
 end
+
+
+def reverse_each_word(array)
+  array.split(" ").collect {|x| x.reverse!}.join(" ")
+end
